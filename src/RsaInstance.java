0bc7be0f576435d94a76e8dc168aff9dd339e429
Robert Alm Nilsson <rorialni@gmail.com>
2016-11-09 13:11:36 +0100

    calculateE and D

--- src/RsaInstance.java
+++ src/RsaInstance.java
@@ -10,13 +10,28 @@ public class RsaInstance {
     Random rng;
 
     public RsaInstance(int size) {
+        if (size < 3) {
+            throw new IllegalArgumentException("Size is too small");
+        }
         rng = new Random();
         this.size = size;
         calculatePrimes();
+        BigInteger pq = p.multiply(q);
+        calculateE();
+        calculateD();
     }
 
     private void calculatePrimes() {
         p = new BigInteger(size, 100, rng);
         q = new BigInteger(size, 100, rng);
+    }
+
+    private void calculateE() {
+        e = new BigInteger(size-1, 100, rng);
+    }
+
+    private void calculateD() {
+        BigInteger p1q1 = p.subtract(new BigInteger("1"))
+            .multiply(q.subtract(new BigInteger("1")));
     }
 }
