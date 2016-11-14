4b105ff8293e47a91d02bfded11f0d15cd3badbc
Robert Alm Nilsson <rorialni@gmail.com>
2016-11-14 07:29:17 +0100

    Move things to a method doEverythingOnce()

--- b/Alice.java
+++ b/Alice.java
@@ -8,79 +8,73 @@ public class Alice {
 	Scanner scan;
 	Socket peerConnectionSocket = null;
 	int proceed ;
-        boolean itReceived = false;
-        BigInteger Confirmer;
         if (args.length != 2) {
             System.err.println("Usage: Server <port> <keysize>");
             return;
         }
         int sizeOfKey = Integer.parseInt(args[1]);
-        RsaInstance rsa = new RsaInstance(sizeOfKey);
-        System.out.println("pq = " + rsa.getPQString());
-        System.out.println("e = " + rsa.getEString());
-        System.out.println("d = " + rsa.getDString());
         PrintWriter pw = null;
         try {
             ServerSocket ss = new ServerSocket(Integer.parseInt(args[0]));
             System.out.println("Waiting for connection...");
             peerConnectionSocket = ss.accept();
-
             pw = new PrintWriter(peerConnectionSocket.getOutputStream());
             scan = new Scanner(peerConnectionSocket.getInputStream());
-            String fromSocket;
-            pw.println(rsa.getEString() + "\n" + rsa.getPQString());
-            pw.flush();
-            System.out.println("Reading encrypted number...");
-	    
-            BigInteger encryptedNumber = new BigInteger(scan.nextLine());
-            System.out.println("Encrypted number = " + encryptedNumber.toString());
-            System.out.println("Decrypting number...");
-            BigInteger decryptedNumber = rsa.decrypt(encryptedNumber);
-            System.out.println("Secret number = " + decryptedNumber.toString());
-           
-	    pw.println(decryptedNumber.toString());
-            pw.flush();  
-            System.out.println("Decoded is sent...");
-            pw.println(sizeOfKey);
-            pw.flush();
-            itReceived = Boolean.parseBoolean(scan.nextLine());
-            if(itReceived){
-		System.out.println("Ready to process reverse keys");
-                BigInteger reverseE = new BigInteger(scan.nextLine());
-                BigInteger reversePQ = new BigInteger(scan.nextLine());
-                RsaInstance rsaReverse = new RsaInstance(reverseE, reversePQ);
-                System.out.println("Done so far");
-                Confirmer = BigInteger.valueOf((int)(Math.random()*100) + 1);
-                System.out.println("Secret number: " + Confirmer.intValue());
-                BigInteger reverseEncrypted =  rsaReverse.encrypt(Confirmer);
-                System.out.println("Alice's encrypted number :" + reverseEncrypted.toString());
-	       
-                pw.println(reverseEncrypted.toString());
-                pw.flush();
-                BigInteger keyFinalised = new BigInteger(scan.nextLine());
-                if(keyFinalised.equals(Confirmer))System.out.println("Secure!");
-                
-            }
-	    //  pw.println();
-            proceed = Integer.parseInt(scan.nextLine());
-            System.out.println("proceeding: "+ proceed);
-	    /*  while (proceed == 1) {
-                System.out.println("Reading encrypted number..");
-                encryptedNumber = new BigInteger(scan.nextLine());
-                System.out.println("Encrypted number = " + encryptedNumber.toString());
-                System.out.println("Decrypting number...");
-                decryptedNumber = rsa.decrypt(encryptedNumber);
-                System.out.println("Secret number = " + decryptedNumber.toString());
-              
+
+            do {
+                doEverythingOnce(pw, scan, sizeOfKey);
                 proceed = Integer.parseInt(scan.nextLine());
                 System.out.println("proceeding: "+ proceed);
-		}*/
-            
+            } while (proceed == 1);
         } catch (IOException e) {
             System.err.println(e.getMessage());
         } finally {
             if (pw != null) {
                 pw.close();
+            }
+        }
+    }
+
+    private static void doEverythingOnce(PrintWriter pw, Scanner scan, int sizeOfKey) {
+        boolean itReceived = false;
+        BigInteger Confirmer;
+        RsaInstance rsa = new RsaInstance(sizeOfKey);
+        System.out.println("pq = " + rsa.getPQString());
+        System.out.println("e = " + rsa.getEString());
+        System.out.println("d = " + rsa.getDString());
+
+        pw.println(rsa.getEString() + "\n" + rsa.getPQString());
+        pw.flush();
+        System.out.println("Reading encrypted number...");
+
+        BigInteger encryptedNumber = new BigInteger(scan.nextLine());
+        System.out.println("Encrypted number = " + encryptedNumber.toString());
+        System.out.println("Decrypting number...");
+        BigInteger decryptedNumber = rsa.decrypt(encryptedNumber);
+        System.out.println("Secret number = " + decryptedNumber.toString());
+
+        pw.println(decryptedNumber.toString());
+        pw.flush();  
+        System.out.println("Decoded is sent...");
+        pw.println(sizeOfKey);
+        pw.flush();
+        itReceived = Boolean.parseBoolean(scan.nextLine());
+        if (itReceived) {
+            System.out.println("Ready to process reverse keys");
+            BigInteger reverseE = new BigInteger(scan.nextLine());
+            BigInteger reversePQ = new BigInteger(scan.nextLine());
+            RsaInstance rsaReverse = new RsaInstance(reverseE, reversePQ);
+            System.out.println("Done so far");
+            Confirmer = BigInteger.valueOf((int)(Math.random()*100) + 1);
+            System.out.println("Secret number: " + Confirmer.intValue());
+            BigInteger reverseEncrypted =  rsaReverse.encrypt(Confirmer);
+            System.out.println("Alice's encrypted number :" + reverseEncrypted.toString());
+
+            pw.println(reverseEncrypted.toString());
+            pw.flush();
+            BigInteger keyFinalised = new BigInteger(scan.nextLine());
+            if (keyFinalised.equals(Confirmer)) {
+                System.out.println("Secure!");
             }
         }
     }
--- b/Bob.java
+++ b/Bob.java
@@ -8,79 +8,69 @@ public class Bob {
 	Scanner scan;
         int proceed;
 	Socket peerConnectionSocket = null;
-        Scanner keyBoard = new Scanner(System.in);
         if (args.length != 2) {
             System.err.println("Usage: Client <address> <port>");
             return;
         }
-        BigInteger unCoded = BigInteger.valueOf((int)(Math.random()*100) + 1);
-        BigInteger Decrypted;
+        Scanner keyboard = new Scanner(System.in);
         PrintWriter pw = null;
         try {
             peerConnectionSocket = new Socket(args[0], Integer.parseInt(args[1]));
-
             pw = new PrintWriter(peerConnectionSocket.getOutputStream());
             scan = new Scanner(peerConnectionSocket.getInputStream());
-            System.out.println("Secret number = " + unCoded.intValue());
-            System.out.println("Waiting for key...");
-            BigInteger keyE = new BigInteger(scan.nextLine());
-            BigInteger keyPQ = new BigInteger(scan.nextLine());
-            RsaInstance rsa = new RsaInstance(keyE, keyPQ);
-            System.out.println("e = " + keyE.toString());
-            System.out.println("pq = " + keyPQ.toString());
-            BigInteger encrypted = rsa.encrypt(unCoded);
-            System.out.println("Encrypted number = " + encrypted.toString());
-            pw.flush();
-            pw.println(encrypted.toString());
-            pw.flush();
-	    Decrypted = new BigInteger(scan.nextLine());
-	       if(Decrypted.equals(unCoded)) {  
-		System.out.println("There is a match");
-                int keySize = Integer.parseInt(scan.nextLine());
-                boolean send = true;
-                pw.println(send);
-                pw.flush();
-                System.out.println("The key size is " + keySize);
-                RsaInstance reverseRSA = new RsaInstance(keySize);
-                System.out.println("pq = " + reverseRSA.getPQString());
-                System.out.println("e = " +  reverseRSA.getEString());
-                System.out.println("d = " +  reverseRSA.getDString());
-                pw.println(reverseRSA.getEString() + "\n" + reverseRSA.getPQString());
-                pw.flush();
-                System.out.println("Recieved encryption: ");
-                BigInteger confirmationEncrypt = new BigInteger(scan.nextLine());
-                System.out.println("Bob's encrypted number: " + confirmationEncrypt.toString());
-                BigInteger confirmationDecrypt = reverseRSA.decrypt(confirmationEncrypt);
-                System.out.println("decrypting...");
-                System.out.println("Alice's secret number: " + confirmationDecrypt.toString());
-                pw.println(confirmationDecrypt.toString());
-                pw.flush(); 
-	 	}
 
-	    //   pw.flush();  
-            System.out.println("Proceed? 1-yes, 0-no");
-            proceed = Integer.parseInt(keyBoard.nextLine());
-            pw.println(proceed);
-	    /*        while (proceed == 1) {
-                unCoded = BigInteger.valueOf((int)(Math.random()*100) + 1);
-                encrypted = rsa.encrypt(unCoded);
-                System.out.println("Encrypted number = " + encrypted.toString());
-                pw.println(encrypted.toString());                 
-                pw.flush();
-                System.out.println("Secret number = " + unCoded.intValue());
-                System.out.println("Waiting for key...");
-
+            do {
+                doEverythingOnce(pw, scan);
                 System.out.println("Proceed? 1-yes, 0-no");
-                proceed=Integer.parseInt(keyBoard.nextLine());
+                proceed = Integer.parseInt(keyboard.nextLine());
                 pw.println(proceed);
                 pw.flush();
-		}*/
+            } while (proceed == 1);
         } catch (IOException e) {
             System.err.println(e.getMessage());
         } finally {
             if (pw != null) {
                 pw.close();
             }
+        }
+    }
+
+    private static void doEverythingOnce(PrintWriter pw, Scanner scan) {
+        BigInteger decrypted;
+        BigInteger unCoded = BigInteger.valueOf((int)(Math.random()*100) + 1);
+        System.out.println("Secret number = " + unCoded.intValue());
+        System.out.println("Waiting for key...");
+        BigInteger keyE = new BigInteger(scan.nextLine());
+        BigInteger keyPQ = new BigInteger(scan.nextLine());
+        RsaInstance rsa = new RsaInstance(keyE, keyPQ);
+        System.out.println("e = " + keyE.toString());
+        System.out.println("pq = " + keyPQ.toString());
+        BigInteger encrypted = rsa.encrypt(unCoded);
+        System.out.println("Encrypted number = " + encrypted.toString());
+        pw.println(encrypted.toString());
+        pw.flush();
+        decrypted = new BigInteger(scan.nextLine());
+        if (decrypted.equals(unCoded)) {
+            System.out.println("There is a match");
+            int keySize = Integer.parseInt(scan.nextLine());
+            boolean send = true;
+            pw.println(send);
+            pw.flush();
+            System.out.println("The key size is " + keySize);
+            RsaInstance reverseRSA = new RsaInstance(keySize);
+            System.out.println("pq = " + reverseRSA.getPQString());
+            System.out.println("e = " +  reverseRSA.getEString());
+            System.out.println("d = " +  reverseRSA.getDString());
+            pw.println(reverseRSA.getEString() + "\n" + reverseRSA.getPQString());
+            pw.flush();
+            System.out.println("Recieved encryption: ");
+            BigInteger confirmationEncrypt = new BigInteger(scan.nextLine());
+            System.out.println("Bob's encrypted number: " + confirmationEncrypt.toString());
+            BigInteger confirmationDecrypt = reverseRSA.decrypt(confirmationEncrypt);
+            System.out.println("decrypting...");
+            System.out.println("Alice's secret number: " + confirmationDecrypt.toString());
+            pw.println(confirmationDecrypt.toString());
+            pw.flush(); 
         }
     }
 }
