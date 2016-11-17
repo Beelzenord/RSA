df4f1367c78820ad0f00d1a592851a569db7d4c9
Robert Alm Nilsson <rorialni@gmail.com>
2016-11-17 16:45:27 +0100

    shortest path

--- lab2/shortestpath.m
+++ lab2/shortestpath.m
@@ -1,6 +1,14 @@
+test1 = [0  7  9  0  0 14
+         7  0 10 15  0  0
+         9 10  0 11  0  2
+         0 15 11  0  6  0
+         0  0  0  6  0  9
+         14 0  2  0  9  0];
+
 tree = [];
 distances = [];
 visited = [];
+shortest_from = [];
 
 function retval = cost_of_edge(v1, v2)
   global tree;
@@ -9,18 +17,21 @@ endfunction
 
 function retval = smallest_unvisited()
   global distances visited;
-  smallest = Inf;
+  smallest_dist = Inf;
+  smallest_node = -1;
   for i = 1:size(distances)
-    if !visited(i) && distances(i) < smallest
-      smallest = distances(i);
+    if !visited(i) && distances(i) < smallest_dist
+      smallest_node = i;
+      smallest_dist = distances(i);
     endif
   endfor
-  retval = smallest;
+  retval = smallest_node;
 endfunction
 
 function retval = shortestpath(t, v1, v2)
-  global tree distances visited;
+  global tree distances visited shortest_from;
   tree = t;
+  shortest_from = ones(size(t, 1), 1) * -1;
   distances = ones(size(t, 1), 1) * Inf;
   visited = [];
   for i = 1:size(tree, 1)
@@ -32,17 +43,24 @@ function retval = shortestpath(t, v1, v2)
   path = [v1];
   while (current < Inf && !visited(v2))
     for neighbor = 1:size(tree, 1)
-      if (visited(neighbor))
+      if (visited(neighbor) || cost_of_edge(current, neighbor) == 0)
         continue;
       endif
       dist_to_neighbor = distances(current) + cost_of_edge(current, neighbor);
       if (dist_to_neighbor < distances(neighbor))
         distances(neighbor) = dist_to_neighbor;
+        shortest_from(neighbor) = current;
       endif
     endfor
     visited(current) = true;
     current = smallest_unvisited();
   endwhile
+  n = v2;
+  while (n != v1)
+    n
+    n = shortest_from(n);
+  endwhile
+  shortest_from
   retval = path;
 endfunction
 
